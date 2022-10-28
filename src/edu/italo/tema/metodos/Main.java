af61485fd2c6d96441931e6f6ea6b64c693a0fe4
Italo <italo.costta26@gmail.com>
2022-10-28 19:52:12 -0300

    exercicios metodos

--- src/edu/italo/tema/metodos/Main.java
+++ src/edu/italo/tema/metodos/Main.java
@@ -5,11 +5,40 @@ public class Main {
 
         // Calculadora
         System.out.println("Exercício calculadora");
-        Calculadora.soma(5, 5);
-        Calculadora.subtracao(5, 2);
-        Calculadora.divisao(6, 3);
-        Calculadora.multiplicacao(10, 2);
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
 
         // Mensagem
+        System.out.println("Exercício mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+        // Empréstimo
+        System.out.println("Exercício empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+
+        // Retornos
+        System.out.println("Exercício retornos");
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Área do quadrado:" + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5,5);
+        System.out.println("Área do retângulo:" + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7,8,9);
+        System.out.println("Área do trapézio:" + areaTrapezio);
     }
 }
